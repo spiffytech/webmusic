d9a3d899b118f13239473ca7eb66bd22f7cbff04
spiffytech <spiffytech@gmail.com>
2015-08-23 16:21:36 -0400

    fsrun: Allow passing in of MONO_OPTIONS

--- fsrun.sh
+++ fsrun.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -i -t -v $(pwd):/source $DOCKEROPTS spiffytech/fsharp-mono-xsp $@
+docker run --rm -i -t -v $(pwd):/source $DOCKEROPTS -e "MONO_OPTIONS=$MONO_OPTIONS" spiffytech/fsharp-mono-xsp $@

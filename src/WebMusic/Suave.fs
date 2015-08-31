fbd9ea392b900a397d9b59af13abb1a5f2c4b337
spiffytech <spiffytech@gmail.com>
2015-08-30 21:12:23 -0400

    Routing! Including returning static files.

--- src/WebMusic/Suave.fs
+++ src/WebMusic/Suave.fs
@@ -2,13 +2,23 @@ namespace WebMusic.Web
 
 module Server2 =
     open Suave
+    open Suave.Http
+    open Suave.Http.Applicatives
     open Suave.Http.Successful
+    open Suave.Http.Files
     open Suave.Web
     open Suave.Sockets
     open System.Net
 
     let serve () =
         //startWebServer {defaultConfig with bindings = [{HttpBinding.scheme=Protocol.HTTP, ip="0.0.0.0", port=9000}]} (OK "Hello, World!")
+        let app =
+            choose
+                [ GET >>= choose
+                    [ path "/" >>= OK "Index!";
+                    pathScan "/static/%s" (fun (filename) -> file (sprintf "./static/%s" filename)) ]
+                ]
+
         startWebServer {defaultConfig with bindings =
             [
                 {defaultConfig.bindings.[0] with socketBinding =
@@ -17,5 +27,5 @@ module Server2 =
                         port=uint16 9000
                     }
                 }
-            ]} (OK "Hello, World!")
+            ]} app
 

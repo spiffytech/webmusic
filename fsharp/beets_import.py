0e57e6d1ff587ba1e63a22c77cd88ebf5b58d941
spiffytech <spiffytech@gmail.com>
2016-07-04 19:09:57 -0400

    fix(beets export): update Beets exporter to support the new track listing format

--- fsharp/beets_import.py
+++ fsharp/beets_import.py
@@ -5,10 +5,29 @@ import json
 import os
 from pprint import pprint
 
-rows = os.popen("cd ~/Music && beet ls -f '$artist	$album	$title	$path'").read().split("\n")
-reader = DictReader(rows, fieldnames=["artist", "album", "title", "path"], delimiter="	")
+formats_map = {
+    "AAC": "aac",
+    "OGG": "ogg",
+    "MP3": "mp3",
+    "FLAC": "flac",
+    "Windows Media": "wma",
+    "ALAC": "alac"
+}
+
+rows = os.popen("cd ~/Music && beet ls -f '$artist	$album	$title	$track	$length	$format	$path'").read().split("\n")
+reader = DictReader(rows, fieldnames=["artist", "album", "title", "track_num", "length", "format", "path"], delimiter="	")
 d = []
 for row in reader:
-    row["path"] = os.path.relpath(row["path"], os.path.join(os.path.expanduser("~"), "Music"))
+    row["formats"] = [{
+        "format": formats_map[row["format"]],
+        "url": os.path.relpath(row["path"], os.path.join(os.path.expanduser("~"), "Music"))
+    }]
+
+    row.pop("format", None)
+    row.pop("path", None)
+
+    row["track_num"] = int(row["track_num"])
+    row["length"] = float(row["length"])
+
     d.append(row)
 json.dump(d, open("/tmp/tracks.json", "w"), indent=4)
